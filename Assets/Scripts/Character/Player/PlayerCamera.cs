1eeebb4c46113706acc529b856450a17e94aa0f7
Anthony Giannotti <72902345+agiannotti@users.noreply.github.com>
2023-09-30 03:33:50 -0500

    .

--- Assets/Scripts/Character/Player/PlayerCamera.cs
+++ Assets/Scripts/Character/Player/PlayerCamera.cs
@@ -8,21 +8,25 @@ namespace Character.Player
         public PlayerManager player;
         public Camera cameraObject;
         [SerializeField] private Transform cameraPivotTransform;
-        [SerializeField] private float leftAndRightLookAngle;
-        [SerializeField] private float upAndDownLookAngle;
+
+        [Header("Camera Settings")]
+        [SerializeField] private float cameraSmoothSpeed = 1;
         [SerializeField] private float leftAndRightRotationSpeed = 75;
         [SerializeField] private float upAndDownRotationSpeed = 1;
         [SerializeField] private float minimumPivot = -30; // lowest point we can look down
         [SerializeField] private float maximumPivot = 60; // highest point we can look up
-
-        [Header("Camera Settings")]
-        private readonly float _cameraSmoothSpeed = 1;
-
+        [SerializeField] private float cameraCollisionRadius = 0.2f;
+        [SerializeField] private LayerMask collideWithLayers;
 
         [Header("Camera Values")]
+        [SerializeField] private float leftAndRightLookAngle;
+        [SerializeField] private float upAndDownLookAngle;
+        private Vector3
+            _cameraObjectPosition; // used for camera collisions ( moves cam object to this position upon colliding )
         private Vector3 _cameraVelocity;
-
+        private float _cameraZPosition; // values used for camera collisions
         private bool _isplayerNotNull;
+        private float _targetCameraZPosition; // values used for camera collisions
 
         private void Awake()
         {
@@ -36,6 +40,34 @@ namespace Character.Player
         {
             _isplayerNotNull = player != null;
             DontDestroyOnLoad(gameObject);
+            _cameraZPosition = cameraObject.transform.localPosition.z;
+        }
+
+        private void HandleCollisions()
+        {
+            _targetCameraZPosition = _cameraZPosition;
+            RaycastHit hit;
+            // direction for collision check
+            var direction = cameraObject.transform.position - cameraPivotTransform.position;
+            direction.Normalize();
+
+            // we check if there is an object in front of our desired direction ^
+            if (Physics.SphereCast(cameraPivotTransform.position, cameraCollisionRadius, direction, out hit,
+                    Mathf.Abs(_targetCameraZPosition), collideWithLayers)) ;
+            {
+                // if there is, we get our distance from it
+                var distanceFromHitObject = Vector3.Distance(cameraPivotTransform.position, hit.point);
+                // we then equate our target z position to the following
+                _targetCameraZPosition = -(distanceFromHitObject - cameraCollisionRadius);
+            }
+
+            // if our target position is less than our collision radius, we subtract our collision radius ( making it snap back! )
+            if (Mathf.Abs(_targetCameraZPosition) < cameraCollisionRadius)
+                _targetCameraZPosition = -cameraCollisionRadius;
+
+            // we then apply our final position using a lerp over a time of 0.2f
+            _cameraObjectPosition.z = Mathf.Lerp(cameraObject.transform.localPosition.z, _targetCameraZPosition, 0.2f);
+            cameraObject.transform.localPosition = _cameraObjectPosition;
         }
 
         public void HandleAllCameraActions()
@@ -46,6 +78,9 @@ namespace Character.Player
             if (!_isplayerNotNull) return;
             HandleFollowTarget();
             HandleRotation();
+
+            // FIXME: Collisions not working :(
+            // HandleCollisions();
         }
 
         private void HandleFollowTarget()
@@ -54,7 +89,7 @@ namespace Character.Player
                 transform.position,
                 player.transform.position,
                 ref _cameraVelocity,
-                _cameraSmoothSpeed * Time.deltaTime);
+                cameraSmoothSpeed * Time.deltaTime);
             transform.position = targetCameraPosition;
         }
 
